ffb4b7c04d929ed76e29f051c4f2b310179f3c27
Shraddhanand <1shraddhanand@gmail.com>
2022-12-31 09:26:00 +0530

    slider start

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="logo">
+          <img src='' alt='logo'></img>
+        </div>
+        <div className='carousel'>
+          <div className=''>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
